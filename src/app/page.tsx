2ed51be381ece5bcb62dda103dda40b0803effbb
Md Rubel Hamza <rubel_xpress@yahoo.com>
2023-07-21 22:17:30 +0600

    add todo, rem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,28 @@
-import TodoItem from '@/components/TodoItem'
-import { prisma } from '@/db'
-import Image from 'next/image'
+import TodoItem from "@/components/TodoItem";
+import { prisma } from "@/db";
+import Image from "next/image";
 
-
-async function todoClicked(id:string,complete:boolean) {
-  "use server"
-  console.log('here..');
+async function todoClicked(id: string, complete: boolean) {
+  "use server";
   const result = await prisma.tODO.update({
-    where:{id}, 
-    data:{complete:!complete}
+    where: { id },
+    data: { complete: !complete },
   });
   console.log(result);
 }
 
 export default async function Home() {
-  
   const todos = await prisma.tODO.findMany();
   return (
     <main>
       <div>Todos</div>
       <div>
         <ul>
-          {
-            todos.map((todo,index)=>(
-              <TodoItem {...todo} key={index} todoClicked={todoClicked} />
-            ))
-          }
+          {todos.map((todo, index) => (
+            <TodoItem {...todo} key={index} todoClicked={todoClicked} />
+          ))}
         </ul>
       </div>
     </main>
-  )
+  );
 }
